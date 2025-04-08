ec084719bd43695a2ccb411264d7be32241a8b5d
VictorIgati <victor.igati@student.moringaschool.com>
2025-04-08 19:20:58 +0300

    Update render.yaml and Pipfile for environment configuration and dependency versions

--- server/build.sh
+++ server/build.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install pipenv and dependencies
+# Install pipenv and upgrade pip
+pip install --upgrade pip
 pip install pipenv
-pipenv install --system --deploy
+
+# Install dependencies and lock file
+rm -f Pipfile.lock  # Remove existing lock file
+pipenv lock  # Generate new lock file
+pipenv install --deploy --system  # Install dependencies
 
 # Initialize flask-migrate
 export FLASK_APP=wsgi.py
