ca2fe5bf9708c969645ec7887b334699ce4a6206
MarkKibet <kemboimarkbiwott@gmail.com>
2025-04-08 13:20:51 +0300

    modified the cases.py

--- server/Models/cases.py
+++
@@ -1,7 +0,0 @@
-from Models import db
-
-class Case(db.Model):
-    __tablename__ = 'cases'
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128), nullable=False)  # Add attributes as needed
