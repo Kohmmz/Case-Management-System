b875be651d75b2f4b050760d2f6c630102254653
Rjamez <robin.adhola@student.moringaschool.com>
2025-04-08 22:35:52 +0300

    ..

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 // client/src/App.js
 import React from 'react';
+import { AuthProvider } from './contexts/AuthContext';  // Import the AuthProvider
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CaseListPage from './pages/CaseListPage';
 import CaseDetailPage from './pages/CaseDetailPage';
-
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/cases" exact component={CaseListPage} />
-        <Route path="/cases/:caseId" component={CaseDetailPage} />
-      </Switch>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route path="/cases" exact component={CaseListPage} />
+          <Route path="/cases/:caseId" component={CaseDetailPage} />
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 };
 
