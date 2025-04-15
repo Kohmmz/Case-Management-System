97dd57e884c97f75f92905083af929e2f560fcc0
MarkKibet <kemboimarkbiwott@gmail.com>
2025-04-15 11:55:21 +0300

    Update cases.py
    Added a comment in line 4 for more clarity

--- server/Models/cases.py
+++ server/Models/cases.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-# Use the same SQLAlchemy instance as in advocates.py
+# Use the same SQLAlchemy instance as in advocates.py, no need to create the instance again
 from .advocates import db  
 
 class Case(db.Model):
